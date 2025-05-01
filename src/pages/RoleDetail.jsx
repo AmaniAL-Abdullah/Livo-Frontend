80fe2a9ea3889448c0b08446a1257050918f8217
Amani Abdullah <amaniabdallah200@gmail.com>
2025-05-02 00:48:57 +0300

    Implement role deletion with confirmation in RoleDetail.jsx

--- src/pages/RoleDetail.jsx
+++ src/pages/RoleDetail.jsx
@@ -9,34 +9,58 @@ function RoleDetail() {
     const navigate = useNavigate()
     const [role, setRole] = useState(null)
     const [errorMsg, setErrorMsg] = useState('')
+    const [deleteConfirm, setDeleteConfirm] = useState(false)
 
     async function getSingleRole() {
-        try {
-            const response = await authorizedRequest('get', `/roles/${id}`)
-            setRole(response.data)
-        } catch (err) {
-            console.log(err)
-            if (err.status === 404) {
-                navigate('/not-found')
-            } else {
-                setErrorMsg('Something went Wrong :-(')
+            try {
+                const response = await authorizedRequest('get', `/roles/${id}`)
+                setRole(response.data)
+            } catch (err) {
+                console.log(err)
+                if (err.status === 404) {
+                    navigate('/not-found')
+                } else {
+                    setErrorMsg('Something went Wrong :-(')
+                }
             }
         }
-    }
 
     useEffect(() => {
-        getSingleRole()
-        console.log(id)
-    }, [])
+            getSingleRole()
+            console.log(id)
+        }, [])
+
+
+    async function deleteRole() {
+            try {
+                const response = await authorizedRequest('delete', `/roles/${id}/`)
+                if (response.status === 204) {
+                    navigate('/')
+                }
+            } catch (err) {
+                console.log(' Role deletion failed:', err)
+            }
+        }
+
+    function showConfirmDelete() {
+            setDeleteConfirm(true)
+        }
 
 
     if (errorMsg) return <h1>{errorMsg}</h1>
-    if (!role) return <h1>Loading your Post...</h1>
+    if (!role) return <h1>Loading your Role...</h1>
 
     return (
         <div>
             <h2>{role.name}</h2>
             <p>{role.description}</p>
+            {
+                deleteConfirm
+                    ?
+                    <button onClick={deleteRole}>Are you Sure?</button>
+                    :
+                    <button onClick={showConfirmDelete}>Delete</button>
+            }
         </div>
     )
 }
